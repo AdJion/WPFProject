e993be96db901466bb5024200b61ecf515a6e65e
Forever Nie <Forever Nie>
2016-05-10 10:39:30 +0800

    添加屏幕截图保存到硬盘中的代码

---
+++ ScreenShot/ScreenShotToFile.cs
@@ -0,0 +1,28 @@
+﻿			BitmapEncoder encoder = new PngBitmapEncoder();
+
+            // create frame from the writable bitmap and add to encoder
+            encoder.Frames.Add(BitmapFrame.Create(this.colorBitmap));
+
+            //string time = System.DateTime.Now.ToString("hh'-'mm'-'ss", CultureInfo.CurrentUICulture.DateTimeFormat);
+
+            string myPhotos = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            string path = Path.Combine(myPhotos, "KinectSnapshot-" + pic_nums + ".png");
+
+            // write the new file to disk
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Create))
+                {
+                    encoder.Save(fs);
+                }
+
+                this.statusBarText.Text = string.Format(CultureInfo.InvariantCulture, "{0} {1}", Properties.Resources.ScreenshotWriteSuccess, path);
+            }
+            catch (IOException)
+            {
+                this.statusBarText.Text = string.Format(CultureInfo.InvariantCulture, "{0} {1}", Properties.Resources.ScreenshotWriteFailed, path);
+            }
+			//在WPF里面如果需要保存SourceImage 到文件中，
+			//先创建了一个Bitmap解码器，然后在帧中添加SourceImage(注意，这里是可擦写的WritableBitmap),
+			//这里的一帧就是一张图片，然后选择需要保存的路径。最后保存这个Bitmap解码器就可以了
